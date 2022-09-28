886b3e0807ab484f169c0fbe98a14f8d9effd49a
velmouth-star <shanpuyangshu@gmail.com>
2022-09-28 13:24:51 +0900

    seed問題解消

--- database/migrations/2022_09_27_090132_create_categories_table.php
+++ database/migrations/2022_09_27_090132_create_categories_table.php
@@ -16,10 +16,6 @@ class CreateCategoriesTable extends Migration
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
             $table->string('title');
-            $table->foreignId('post_id')
-                ->constrained('')
-                ->cascadeOnUpdate()
-                ->cascadeOnDelete();
             $table->timestamps();
         });
     }
--- database/migrations/2022_09_27_144031_create_posts_table.php
+++ database/migrations/2022_09_27_144031_create_posts_table.php
@@ -22,6 +22,10 @@ class CreatePostsTable extends Migration
                 ->constrained()
                 ->cascadeOnUpdate()
                 ->cascadeOnDelete();
+            $table->foreignId('category_id')
+                ->constrained()
+                ->cascadeOnUpdate()
+                ->cascadeOnDelete();
             $table->timestamps();
         });
     }
